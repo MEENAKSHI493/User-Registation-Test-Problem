f9f76b929d171443721b3123a9c90c27d9c005ed
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-07-16 23:09:19 +0530

    Enter valid last name

--- src/test/java/UserRegistration/UserRegistrationTest.java
+++ src/test/java/UserRegistration/UserRegistrationTest.java
@@ -4,7 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.*;
 
-
 public class UserRegistrationTest {
 	UserReg userReg;
 
@@ -13,6 +12,8 @@ public class UserRegistrationTest {
 		userReg = new UserReg();
 
 	}
+
+	// Check First Name
 	@Test
 	public void givenName_WhenFirstNameCaptial_ShouldReturn() {
 
@@ -40,17 +41,58 @@ public class UserRegistrationTest {
 		boolean actual = userReg.isFirstName("Meenakshi123");
 		Assert.assertFalse(actual);
 	}
-	
+
 	@Test
 	public void givenName_whenFirstName_Havespace() {
 		boolean actual = userReg.isFirstName("Meen  akshi");
 		Assert.assertFalse(actual);
 	}
-	
+
 	@Test
 	public void givenName_whenFirstName_givenspace_haveNumber() {
 		boolean actual = userReg.isFirstName("Meenakshi 123");
 		Assert.assertFalse(actual);
-	
-}
+
 	}
+
+	// Check Last Name
+
+	@Test
+	public void givenName_WhenLastNameCaptial_ShouldReturn() {
+
+		boolean actual = userReg.isLastName("Budarkar");
+		Assert.assertTrue(actual);
+	}
+
+	@Test
+	public void givenName_WhenLastName_FirstCharacter_IsInLowerCase() {
+
+		boolean actual = userReg.isLastName("budarkar");
+		Assert.assertFalse(actual);
+	}
+
+	@Test
+	public void givenName_WhenLastName_SpecialCharacter_IsThere_OrNot() {
+
+		boolean actual = userReg.isLastName("Budarkar@");
+		Assert.assertFalse(actual);
+	}
+
+	@Test
+	public void givenName_WhenLastName_HaveNumbers() {
+
+		boolean actual = userReg.isLastName("Budarkar123");
+		Assert.assertFalse(actual);
+	}
+
+	@Test
+	public void givenName_whenLastName_Havespace() {
+		boolean actual = userReg.isFirstName("Budar kar");
+		Assert.assertFalse(actual);
+
+}
+	public void givenName_whenLastName_givenspace_haveNumber() {
+		boolean actual = userReg.isFirstName("Budarkar 123");
+		Assert.assertFalse(actual);
+	}
+}
