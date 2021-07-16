bfd7c76872b9fb59aad5e88d6580c64a32965a03
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-07-16 22:53:06 +0530

    Enter vaid first name

--- src/main/java/UserRegistration/UserReg.java
+++ src/main/java/UserRegistration/UserReg.java
@@ -1,5 +1,17 @@
 package UserRegistration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserReg {
+	
+	public static boolean isFirstName(String firstname) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern patt = Pattern.compile(regex);
+		if (firstname == null) {
+			return false;
+		}
+		Matcher match = patt.matcher(firstname);
+		return match.matches();
+	}
 
 }
--- src/test/java/UserRegistration/UserRegistrationTest.java
+++ src/test/java/UserRegistration/UserRegistrationTest.java
@@ -1,5 +1,56 @@
 package UserRegistration;
 
-public class UserRegistrationTest {
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.*;
 
+
+public class UserRegistrationTest {
+	UserReg userReg;
+
+	@Before
+	public void setUp() {
+		userReg = new UserReg();
+
+	}
+	@Test
+	public void givenName_WhenFirstNameCaptial_ShouldReturn() {
+
+		boolean actual = userReg.isFirstName("Meenakshi");
+		Assert.assertTrue(actual);
+	}
+
+	@Test
+	public void givenName_WhenFirstName_FirstCharacter_IsInLowerCase() {
+
+		boolean actual = userReg.isFirstName("meenakshi");
+		Assert.assertFalse(actual);
+	}
+
+	@Test
+	public void givenName_WhenFirstName_SpecialCharacter_IsThere_OrNot() {
+
+		boolean actual = userReg.isFirstName("Meenakshi@");
+		Assert.assertFalse(actual);
+	}
+
+	@Test
+	public void givenName_WhenFirstName_HaveNumbers() {
+
+		boolean actual = userReg.isFirstName("Meenakshi123");
+		Assert.assertFalse(actual);
+	}
+	
+	@Test
+	public void givenName_whenFirstName_Havespace() {
+		boolean actual = userReg.isFirstName("Meen  akshi");
+		Assert.assertFalse(actual);
+	}
+	
+	@Test
+	public void givenName_whenFirstName_givenspace_haveNumber() {
+		boolean actual = userReg.isFirstName("Meenakshi 123");
+		Assert.assertFalse(actual);
+	
 }
+	}
